4ab14810d6021b0af4c2ec5d1a066f03bd5a6ca5
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2024-05-01 06:55:36 -0400

    reduced html load

---
+++ js/components/generateStages.js
@@ -0,0 +1,18 @@
+const stageOptionsContainer = document.getElementById("stageOptionsContainer");
+
+const generateStageOptions = () => {
+    for (let i = 1; i < 6; i++) {
+        stageOptionsContainer.innerHTML += `
+      <a
+        class="dropdown-item text-light"
+        onclick="setStage('${i}')"
+        id="stage${i}Select"
+        href=""
+      >
+        ${i}
+      </a>
+      `;
+    }
+};
+
+generateStageOptions();
