c34d40b6a52569eb14e0b58d9a7359397303ff50
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2026-07-03 07:02:29 -0400

    Update missions.js

--- js/constants/missions.js
+++ js/constants/missions.js
@@ -273,8 +273,8 @@ const MISSIONS = {
       ostChipReward: null,
     },
     {
-      name: "Escort the Weaponized Mining Ship",
-      challenge: "Complete the mission without using more than one repair kit.",
+      name: "Destroy the Weaponized Mining Ship",
+      challenge: "Complete the mission without using any repair kits.",
       chapter: 1,
       ostChipReward: null,
     },
