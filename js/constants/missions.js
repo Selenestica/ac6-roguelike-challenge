c686697e00f04e8537f2f1a2eb1b23e3066f7ccb
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2026-09-09 08:27:30 -0400

    changed optional challenge of spaceport mission

--- js/constants/missions.js
+++ js/constants/missions.js
@@ -117,7 +117,7 @@ const MISSIONS = {
     {
       name: "Attack the Refueling Base",
       challenge:
-        "Destroy all 16 fuel tanks, defeat all 5 LCs and complete the mission without resupplying.",
+        "Destroy defeat all 5 LCs and complete the mission without resupplying.",
       chapter: 3,
       ostChipReward: null,
     },
@@ -423,7 +423,7 @@ const MISSIONS = {
     {
       name: "Attack the Refueling Base",
       challenge:
-        "Destroy all 16 fuel tanks, defeat all 5 LCs and complete the mission without resupplying.",
+        "Destroy defeat all 5 LCs and complete the mission without resupplying.",
       chapter: 3,
       ostChipReward: null,
     },
@@ -701,7 +701,7 @@ const MISSIONS = {
     {
       name: "Attack the Refueling Base",
       challenge:
-        "Destroy all 16 fuel tanks, defeat all 5 LCs and complete the mission without resupplying.",
+        "Destroy defeat all 5 LCs and complete the mission without resupplying.",
       chapter: 3,
       ostChipReward: null,
     },
